b5cb702e683a29d43a872c314fbb3490501f5c8e
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-09-16 15:38:34 -0300

    Update arq.c

--- arq.c
+++ arq.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 #define MAX 100
 #include <stdlib.h>
 //CVh = [( (comprimento em (mm) + 19) / 25, 4) × 3] − 24 massculina
@@ -11,7 +11,7 @@ ser comprada para o gênero G, em que G recebe 0 para se referir ao gênero
 masculino e 1 para feminino.
 1 = fe,inino
 0 = masculino*/
-int feminino(int aux){
+/*int feminino(int aux){
     int covert = ((aux + 19) / 25,4) × 3) − 23 ;
     printf("F %d", aux) ;
     return 0;
@@ -52,6 +52,59 @@ int main(){
         teste -- ;
     }
     return 0 ; 
+}*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+/*1 = feminino
+0 = masculino*/
+
+void feminino(int a){
+    double convert = (((a + 19)/25,4) * 3) - 23 ;
+    printf("F %lf\n", convert) ;
 }
 
-/*
+void masculino(int b){    
+    double convert = (((b + 19)/25,4) * 3) - 24 ;
+    printf("M %lf\n", convert) ;
+}
+
+int main(){
+  int l[] = {3, 35, 1, 40, 1, 40, 1} ;
+  int i, j, teste, cont = 0, aux = 0, size = 0 ; 
+  teste = l[0] ;
+  size = sizeof(l) / sizeof(l[0]) ;
+  printf ("%d\n", teste) ;
+  printf ("%d\n", size) ;
+
+  for (i = 2 ; i <= size ; i += 2){
+    if (l[i] == 1){
+      aux = l[i-1] ;
+      feminino(aux) ;
+    }else
+      if (l[i] == 0){
+        aux = l[i-1] ;
+        masculino(aux) ;
+      }
+  }
+/*  while(cont <= teste)
+  {
+    for (i = 1 ; i <= (teste * 2) ; i+2){
+      for (j = 2 ; j <= (teste * 2) ; j+2){
+        if (j == 0)
+        {
+          aux = l[i] ;
+          masculino(aux) ;
+        }else
+        if (j == 1)
+        {
+          aux = l[i] ;
+          feminino(aux) ;
+        }
+      }
+    }
+    cont++ ;
+  }*/
+    return 0 ; 
+}
