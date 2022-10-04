ff3807e041e548827cfb91ea8d7414c8f220e8a2
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-10-04 12:28:20 -0300

    Update arq.c

--- arq.c
+++ arq.c
@@ -30,3 +30,44 @@ void main ()
         printf ("Nao Leu") ;
     }
 }
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+
+typedef struct{
+    char descricao[50];
+    int preco;
+    int quantidade;
+}produto ;
+
+void main ()
+{
+    produto reg [MAX];
+    int i ;
+    FILE *arq ;
+    arq = fopen("myfile1.bin", "r+b") ;
+    fseek(arq, 0, SEEK_END) ;
+    char recovery[1000] ;
+    if (arq != NULL)
+    {
+        for (i = 0 ; i < MAX ; i++)
+        {
+            fscanf(arq,"%s %s", recovery, reg[i].descricao) ;
+            fscanf(arq,"%s %d", recovery, &reg[i].preco) ;
+            fscanf(arq,"%s %d", recovery, &reg[i].quantidade) ;
+        }
+        fclose(arq) ;
+    }else
+    {
+        printf ("Nao Leu") ;
+    }
+    for (i = 0 ; i < MAX ; i++)
+    {
+        printf ("Descricao: %s ", reg[i].descricao) ;
+        printf ("Preco: %d ", reg[i].preco) ;
+        printf ("Qtd: %d\n", reg[i].quantidade) ;
+    }
+    printf ("Recovery: %s\n", recovery) ;
+}
+*/
