665709d5fddb73f5e0e7221e616f9b15be1afccb
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-10-23 22:40:06 -0300

    Update lab02.c

--- lab02.c
+++ lab02.c
@@ -274,3 +274,122 @@ int main ()
     
     return 0 ;
 }
+
+
+/* Programa: Laboratório 2
+   Autor: Gustavo Henrique Tavares Isobe
+   Versao: 1.0 - 13/10/2022
+*/
+
+// ##################### Bibliotecas Externas ##############################
+ 
+#include<stdio.h>
+#include<stdlib.h> 
+#define MAX 100
+
+// ########################## TAD X.h ######################################
+
+struct info
+{
+    int valor;
+    int prox;
+};
+typedef struct info tipoInfo;
+
+struct listaEE
+{
+    int tamanhoLista;
+    int primeiro ;
+    int ultimo ;
+    int posLivre[MAX];
+    tipoInfo elemento[MAX];
+};
+typedef struct listaEE tipoListaEE;
+
+void inicializaLista (tipoListaEE *listaAux, int t)
+{
+    listaAux->tamanhoLista = 0 ;
+    listaAux->primeiro = -1 ;
+    listaAux->ultimo = -1;
+    for (int i = 0 ; i < t ; i++)
+    {
+        listaAux->elemento[i].valor = 0 ;
+        listaAux->elemento[i].prox = -1 ;
+        listaAux-> posLivre[i] = 1 ; 
+    }
+}
+
+void insereElementoFinal (tipoListaEE *listaAux, int posicao)
+{ 
+    if (posicao != -1)
+    {
+        scanf("%d", &listaAux->elemento[posicao].valor) ;
+        if (listaAux->tamanhoLista == 0)  //atualizando dados do nó da lista
+            listaAux->primeiro = posicao ;
+        else listaAux->elemento[listaAux->ultimo].prox = posicao ;
+        listaAux->elemento[posicao].prox = -1 ;
+        listaAux->tamanhoLista++ ;
+        listaAux->ultimo = posicao ;
+        listaAux->posLivre[posicao] = 0 ;
+        printf ("Deu certo\n") ;
+    }
+}
+
+int proxPosLivre(tipoListaEE *listaAux, int t)
+{
+    for (int i = 0 ; i < t ; i++)
+    {
+        if (listaAux->posLivre[i] == 1)
+            return i ;
+    }
+    return -1;
+}
+
+
+
+//por ultimo:
+void imprimeLista (tipoListaEE *l)
+{
+  tipoListaEE *p ;
+  int i = 0 ;
+  for (p = l; p != NULL ; p = p->elemento[i].prox)
+  {
+    printf("info = %d\n", p->elemento[i].valor) ;
+    i++ ;
+  }
+}
+
+// ###################### Funcoes e Procedimentos do programa ##############
+
+void ano_de_ingresso()
+{
+  //printar ano de ingresso e qtd.
+}
+
+// ############################ Principal ###################################
+
+int main ()
+{
+  tipoInfo noListaEE ;
+  tipoListaEE listaEE ;
+  int t, i, aux, operacao ;
+  scanf ("%d", &t) ;
+  t = t * 2 ;
+  if (t > 0)
+  {
+    inicializaLista(&listaEE, t) ;
+    for (i = 0 ; i < t ; i++)
+    {
+      scanf("%d", &operacao) ;
+      if (operacao == 1){
+        aux = proxPosLivre(&listaEE, t) ;
+        insereElementoFinal(&listaEE, aux) ;
+      }else if (operacao == 2){
+            printf("Retirar\n") ;
+      }
+    }
+  }else
+    printf ("Nao\n") ;
+    
+    return 0 ;
+}
