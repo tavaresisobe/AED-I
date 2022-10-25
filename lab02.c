b2e6998abfc5fbd153ebb00b71f04fd10ee4d19d
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-10-25 15:32:38 -0300

    Update lab02.c

--- lab02.c
+++ lab02.c
@@ -2,6 +2,7 @@
 http://www.sergioportari.com.br/wp-content/uploads/2018/10/aula05-Listas-estaticas.pdf
 https://www.sergioportari.com.br/aulas/2018-2/uemg-frutal/algoritmos-e-estruturas-de-dados-aed/
 */
+
 /* Programa: Laboratório 2
    Autor: Gustavo Henrique Tavares Isobe
    Versao: 1.0 - 13/10/2022
@@ -14,182 +15,7 @@ https://www.sergioportari.com.br/aulas/2018-2/uemg-frutal/algoritmos-e-estrutura
 #define MAX 100
 
 // ########################## TAD X.h ######################################
- 
-struct info
-{
-    int valor;
-    int prox;
-};
-typedef struct info tipoInfo;
 
-struct listaEE
-{
-    int tamanhoLista;
-    int primeiro;
-    int ultimo;
-    int posLivre[MAX];
-    tipoInfo elemento[MAX];
-};
-typedef struct listaEE tipoListaEE;
-
-void inicializaLista (tipoListaEE *listaAux)
-{
-    listaAux->tamanhoLista = 0 ;
-    listaAux->primeiro = -1 ;
-    listaAux->ultimo = -1;
-    for (i = 0 ; i < N ; i++)
-    {
-        listaAux->elemento[MAX].valor = 0 ;
-        listaAux->elemento[MAX].prox = -1 ;
-        listaAux-> posLivre[MAX] = 1 ; 
-    }
-}
-
-void insereElementoFinal (tipoListaEE *listaAux, int posicao)
-{ 
-    if (posicao!=-1)
-    {
-        scanf("%d", &listaAux->elemento[posicao].valor) ;
-        if (listaAux->tamanhoLista == 0) 
-            listaAux->primeiro=posicao ;
-        else
-            listaAux->elemento[listaAux->ultimo].prox = posicao;
-            listaAux->elemento[posicao].prox=-1;
-            listaAux->tamanhoLista++;
-            listaAux->ultimo=posicao;
-            listaAux->posLivre[posicao] = 0 ; 
-    }
-}
-
-int proxPosLivre(tipoListaEE *listaAux)
-{
-    for (i=0;i<N;i++)
-    {
-        if (listaAux->posLivre[i]==1)
-            return i;
-    }
-    return -1;
-}
-// ###################### Funcoes e Procedimentos do programa ##############
-
- 
-
-// ############################ Principal ###################################
-
-int main ()
-{
-    int 
-    
-    
-    
-    return 0 ;
-}
-
-/*
-/* Programa: Laboratório 2
-   Autor: Gustavo Henrique Tavares Isobe
-   Versao: 1.0 - 13/10/2022
-*/
-
-// ##################### Bibliotecas Externas ##############################
- 
-#include<stdio.h>
-#include<stdlib.h> 
-#define MAX 100
-
-// ########################## TAD X.h ######################################
- 
-struct info
-{
-    int valor;
-    int prox;
-};
-typedef struct info tipoInfo;
-
-struct listaEE
-{
-    int tamanhoLista;
-    int primeiro;
-    int ultimo;
-    int posLivre[MAX];
-    tipoInfo elemento[MAX];
-};
-typedef struct listaEE tipoListaEE;
-
-void inicializaLista (tipoListaEE *listaAux)
-{
-    listaAux->tamanhoLista = 0 ;
-    listaAux->primeiro = -1 ;
-    listaAux->ultimo = -1;
-    for (i = 0 ; i < N ; i++)
-    {
-        listaAux->elemento[MAX].valor = 0 ;
-        listaAux->elemento[MAX].prox = -1 ;
-        listaAux-> posLivre[MAX] = 1 ; 
-    }
-}
-
-void insereElementoFinal (tipoListaEE *listaAux, int posicao)
-{ 
-    if (posicao!=-1)
-    {
-        scanf("%d", &listaAux->elemento[posicao].valor) ;
-        if (listaAux->tamanhoLista == 0) 
-            listaAux->primeiro=posicao ;
-        else
-            listaAux->elemento[listaAux->ultimo].prox = posicao;
-            listaAux->elemento[posicao].prox=-1;
-            listaAux->tamanhoLista++;
-            listaAux->ultimo=posicao;
-            listaAux->posLivre[posicao] = 0 ; 
-    }
-}
-
-int proxPosLivre(tipoListaEE *listaAux)
-{
-    for (i=0;i<N;i++)
-    {
-        if (listaAux->posLivre[i]==1)
-            return i;
-    }
-    return -1;
-}
-// ###################### Funcoes e Procedimentos do programa ##############
-
-void listacheia()
-{
-    
-}
-
-void remocao()
-{
-    
-}
-
-// ############################ Principal ###################################
-
-int main ()
-{
-    int 
-    
-    
-    
-    return 0 ;
-}*/
-   
-/* Programa: Laboratório 2
-   Autor: Gustavo Henrique Tavares Isobe
-   Versao: 1.0 - 13/10/2022
-*/
-
-// ##################### Bibliotecas Externas ##############################
- 
-#include<stdio.h>
-#include<stdlib.h> 
-#define MAX 100
-
-// ########################## TAD X.h ######################################
- 
 struct info
 {
     int valor;
@@ -220,21 +46,73 @@ void inicializaLista (tipoListaEE *listaAux, int t)
     }
 }
 
-void insereElementoFinal (tipoListaEE *listaAux, int posicao)
-{ 
-    if (posicao != -1)
+void insereElementoOrdenado (tipoListaEE *listaAux, int posicao)
+{
+  int x ;
+  scanf("%d", &x) ;
+  if (posicao != -1 && x >= 60000 && x<= 140000)
+  {
+    listaAux->elemento[posicao].valor = x ;
+    if (listaAux->tamanhoLista == 0) //caso seja o 1a
     {
-        scanf("%d", &listaAux->elemento[posicao].valor) ;
-        if (listaAux->tamanhoLista == 0) 
-            listaAux->primeiro = posicao ;
-        else listaAux->elemento[listaAux->ultimo].prox = posicao ;
-        listaAux->elemento[posicao].prox = -1 ;
-        listaAux->tamanhoLista++ ;
-        listaAux->ultimo = posicao ;
-        listaAux->posLivre[posicao] = 0 ;
-        printf ("Deu certo\n") ;
+      listaAux->primeiro = posicao ;
+      listaAux->ultimo = posicao ;
+      listaAux->elemento[posicao].prox = -1 ;
+      listaAux->tamanhoLista++ ;
+      listaAux->posLivre[posicao] = 0 ;
+      printf("inseriu o primeiro elemento\n") ;
+    }else if (listaAux->tamanhoLista > 0 && listaAux->elemento[posicao].valor > listaAux->elemento[listaAux->ultimo].valor){ //caso seja maior que o ultimo                 
+      listaAux->elemento[listaAux->ultimo].prox = posicao ;
+      listaAux->tamanhoLista++ ;
+      listaAux->elemento[posicao].prox = -1 ;
+      listaAux->posLivre[posicao] = 0 ;
+      listaAux->ultimo = posicao ;
+      printf ("Inseriu no ultimo\n") ;
+    }else if (listaAux->tamanhoLista > 0 && listaAux->elemento[posicao].valor < listaAux->elemento[listaAux->primeiro].valor){  //caso seja menor que o primeiro
+      listaAux->elemento[posicao].prox = listaAux->primeiro ;
+      listaAux->tamanhoLista++ ;
+      listaAux->posLivre[posicao] = 0 ;
+      listaAux->primeiro = posicao ;
+      printf ("Inseriu no primeiro\n") ;
+    }else if (listaAux->tamanhoLista > 1){  //insercao intermediaria ordenada
+      int aux ;
+      for (int i = 0 ; i < listaAux->tamanhoLista ; i++)
+      { 
+        if (listaAux->elemento[posicao].valor > listaAux->elemento[i].valor)
+        {
+          aux = listaAux->elemento[i].prox ;
+          if (listaAux->elemento[aux].valor > listaAux->elemento[posicao].valor)
+          {
+            listaAux->elemento[i].prox = posicao ;
+            listaAux->elemento[posicao].prox = aux ;
+            listaAux->tamanhoLista++ ;
+            listaAux->posLivre[posicao] = 0 ;
+            break ;
+          }
+        }
+      }
     }
+  }else
+      printf ("Nao existe\n") ;
 }
+
+/*void removerElemento (tipoListaEE *listaAux, int t)
+{
+    int x, i, tam ;
+    scanf ("%d", &x) ;
+    if (x >= 60000 && x <= 140000)
+    {
+        tam = listaAux->tamanhoLista ; 
+        while(tam > 0)
+        {
+            if ()
+            
+            
+            tam -- ;
+        }
+    }else
+        prinf ("Nao existe\n") ;
+}*/
 
 int proxPosLivre(tipoListaEE *listaAux, int t)
 {
@@ -245,11 +123,25 @@ int proxPosLivre(tipoListaEE *listaAux, int t)
     }
     return -1;
 }
+
+//por ultimo:
+void imprimeListaEE (tipoListaEE *l)
+{
+  int i = 0, aux = l->primeiro, tam = l->tamanhoLista ;
+  while (tam > 0)
+  {
+    printf ("%d ", l->elemento[aux].valor) ;
+    aux = l->elemento[aux].prox ;
+    tam -- ;
+  }
+  printf("\n") ;
+}
+
 // ###################### Funcoes e Procedimentos do programa ##############
 
-void ano_de_ingresso()
+void ano_de_ingresso(tipoListaEE *listaAux)
 {
-  //printar qtd dos ra's e ano de ingresso.
+  //printar ano de ingresso e qtd.
 }
 
 // ############################ Principal ###################################
@@ -258,23 +150,30 @@ int main ()
 {
   tipoInfo noListaEE ;
   tipoListaEE listaEE ;
-  int t, i, aux ;
+  int t, i, aux, operacao ;
   scanf ("%d", &t) ;
   t = t * 2 ;
   if (t > 0)
   {
     inicializaLista(&listaEE, t) ;
     for (i = 0 ; i < t ; i++)
-    { 
-      aux = proxPosLivre(&listaEE, t) ;
-      insereElementoFinal(&listaEE, aux) ;
+    {
+      scanf("%d", &operacao) ;
+      if (operacao == 1){
+        aux = proxPosLivre(&listaEE, t) ;
+        printf ("posLivre: %d\n" , aux) ;
+        insereElementoOrdenado(&listaEE, aux) ;
+        imprimeListaEE(&listaEE) ;
+      }else if (operacao == 2){
+            //removerElemento(&listaEE, t) ;
+      }
     }
+    ano_de_ingresso(&listaEE) ;
   }else
-    printf ("Nao\n") ;
+    ano_de_ingresso(&listaEE) ;
     
     return 0 ;
 }
-
 
 /* Programa: Laboratório 2
    Autor: Gustavo Henrique Tavares Isobe
