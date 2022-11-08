8537cf416464df0c5a6bbcc487dd5d1b813e6175
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-11-07 22:40:11 -0300

    Update lab04.c

--- lab04.c
+++ lab04.c
@@ -55,3 +55,99 @@ int main(void)
   
   return 0;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct elemento {
+int info;
+struct elemento *prox;
+struct elemento *ant;
+};
+
+typedef struct elemento tipoElemento;
+
+struct estruturaLDDE{
+tipoElemento *primeiro;
+tipoElemento *ultimo;
+int tamanhoLista;
+};
+
+typedef struct estruturaLDDE tipoLDDE;
+
+void inicializaLista (tipoLDDE *listaAux)
+{
+  listaAux->primeiro = NULL;
+  listaAux->ultimo = NULL;
+  listaAux->tamanhoLista = 0;
+}
+
+void insereElementoFinal (tipoLDDE *listaAux)
+{ // Alocando espaço para o elemento
+  tipoElemento *novo = (tipoElemento*) malloc(sizeof(tipoElemento));
+  // fazendo leitura dos dados para info
+  scanf("%d", &novo->info);
+  if (listaAux->tamanhoLista == 0){
+    novo->prox = NULL;
+    novo->ant = NULL;
+    listaAux->primeiro=novo;
+    listaAux->ultimo=novo;
+  }else{
+    novo->prox = NULL;
+    novo->ant = listaAux->ultimo;
+    listaAux->ultimo->prox = novo;
+    listaAux->ultimo=novo; 
+    }
+    listaAux->tamanhoLista++; 
+}
+
+void insereJogada(tipoLDDE *listaAux)
+{
+  // Alocando espaço para o elemento
+  tipoElemento *novo = (tipoElemento*) malloc(sizeof(tipoElemento));
+  // fazendo leitura dos dados para info
+  scanf("%d", &novo->info);
+  if (listaAux->tamanhoLista == 0){
+    novo->prox = NULL;
+    novo->ant = NULL;
+    listaAux->primeiro=novo;
+    listaAux->ultimo=novo;
+  }else{
+    novo->prox = NULL;
+    novo->ant = listaAux->ultimo;
+    listaAux->ultimo->prox = novo;
+    listaAux->ultimo=novo; 
+    }
+    listaAux->tamanhoLista++; 
+}
+
+void checaTripla(tipoLDDE *listaAux)
+{
+  
+}
+
+void listarLista(tipoLDDE *listaAux)
+{
+  
+}
+
+int main(void) 
+{
+  tipoLDDE listaLDDE ; 
+  tipoElemento lInfo ;
+  inicializaLista (&listaLDDE) ;
+  for (int i = 0 ; i < 9 ; i++)
+  {
+    insereElementoFinal(&listaLDDE) ;  
+  }
+  for (int i = 0 ; i < 5 ; i++)
+  {
+    insereJogada(&listaLDDE) ;
+    checaTripla(&listaLDDE) ;
+    listarLista(&listaLDDE) ;
+  }
+ 
+  
+  return 0;
+  }
