4ac0a9399892b54930cc6587a6bd19b263f8342a
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-11-16 22:14:02 -0300

    Create lab05.c

---
+++ lab05.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define tamanhoMAX 100
+
+struct filaCircular{
+  int primeiro;
+  int ultimo;
+  int tamanhofila;
+  int elementos[tamanhoMAX];
+};
+typedef struct filaCircular tipoFC;
+
+void inicializaFila(tipoFC *filaAux) {
+  // inicializa a fila
+  int i;
+  filaAux->primeiro = -1;
+  filaAux->ultimo = -1;
+  for (i = 0; i < tamanhoMAX; i++)
+    filaAux->elementos[i] = 0;
+}
+
+void insereElemento(tipoFC *filaAux,int info) { // se a fila estiver vazia insere o primeiro elemento
+  if (filaVazia(filaAux) == 1) {
+    filaAux->primeiro = 0 ;
+    filaAux->ultimo = 0 ;
+    filaAux->elementos[0] = info ;
+  }else if (filaCheia(filaAux) == 0) { // se a fila não estiver cheia podemos inserir
+    int posicao = ((filaAux->ultimo + 1) % tamanhoMAX) ;
+    filaAux->elementos[posicao] = info ;
+    filaAux->ultimo = posicao ;
+  }else
+    printf("overflow: fila cheia") ;
+}
+
+int filaVazia(tipoFC *filaAux) {
+  if ((filaAux->primeiro == filaAux->ultimo) && (filaAux->ultimo == -1))
+    return 1 ; // fila está vazia
+  else
+    return 0 ; // fila não está vazia
+}
+
+int filaCheia(tipoFC *filaAux) {
+  if (((filaAux->ultimo + 1) % tamanhoMAX) == filaAux->primeiro)
+    return 1 ; // fila está cheia
+  else
+    return 0 ; // fila não está cheia
+}
+
+int main(void) {
+  tipoFC filaEsC ;
+  int i, x, op = 0, num ;
+  scanf("%d", &x) ;
+  if (x > 0) 
+  {
+    inicializaFila(&filaEsC) ;
+    for (i = 0; i < x; i++)
+    {
+      scanf("%d", &op) ;
+      if (op == 1) 
+      {
+        scanf ("%d", &num) ;
+        insereElemento(&filaEsC, num) ;
+      }if (op == 2)
+      {
+        printf ("Vai Corinthians\n");
+      }if (op == 3)
+      {
+        printf ("Palmeiras não tem mundial\n");
+      }
+    }
+  }
+  return 0;
+}
