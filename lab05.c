f7c04f2247b776717e858b28fb6d969ded2e76ae
Gustavo Tavares Isobe <91920423+tavaresisobe@users.noreply.github.com>
2022-11-20 21:34:01 -0300

    Update lab05.c

--- lab05.c
+++ lab05.c
@@ -368,3 +368,145 @@ int main(void) {
   listarFila(&filaEsC) ;
   return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#define tamanhoMAX 20
+
+struct filaCircular {
+    int primeiro;
+    int ultimo;
+    int tamanhofila;
+    int elementos[tamanhoMAX];
+};
+typedef struct filaCircular tipoFC;
+
+void inicializaFila (tipoFC *filaAux)
+{
+// inicializa a fila
+    int i ;
+    filaAux->primeiro = -1;
+    filaAux->ultimo = -1;
+    for (i = 0 ; i < tamanhoMAX ; i++)
+    {
+        filaAux->elementos[i] = 0;
+    }
+}
+
+int filaVazia(tipoFC *filaAux){
+    if ((filaAux->primeiro==filaAux->ultimo) && (filaAux->ultimo==-1))
+        return 1; // fila está vazia
+    else return 0; // fila não está vazia
+}
+
+int filaCheia (tipoFC *filaAux){
+    if (((filaAux->ultimo + 1) % tamanhoMAX) == filaAux->primeiro)
+        return 1; // fila está cheia
+    else return 0; // fila não está cheia
+}
+    
+void insereElemento (tipoFC *filaAux, int info)
+{ // se a fila estiver vazia insere o primeiro elemento
+    if (filaVazia(filaAux) == 1)
+    { filaAux->primeiro = 0 ;
+      filaAux->ultimo = 0 ;
+      filaAux->elementos[0] = info ;
+      filaAux->tamanhofila ++ ;
+    }else if (filaCheia(filaAux) == 0){ // se a fila não estiver cheia podemos inserir
+        int posicao = ((filaAux->ultimo + 1) % tamanhoMAX) ;
+        filaAux->elementos[posicao] = info;
+        filaAux->ultimo = posicao;
+        filaAux->tamanhofila ++ ;
+    }else printf("overflow: fila cheia");
+}
+
+void removerElemento (tipoFC *filaAux)
+{
+    if (filaVazia(filaAux) == 0)
+    {
+        filaAux->elementos[filaAux->primeiro] = 0 ;
+        filaAux->tamanhofila -- ;
+        if (filaAux->tamanhofila == 0)
+        {
+            filaAux->primeiro = -1 ;
+            filaAux->ultimo = -1 ;
+        }else
+            filaAux->primeiro + 1 ;
+    }else
+        printf ("vazia\n") ;
+}
+
+void finalizarTarefa (tipoFC *filaAux)
+{
+    int posicao = ((filaAux->ultimo + 1) % tamanhoMAX) ;
+    if (filaVazia(filaAux) == 0)
+    {
+        if (filaAux->tamanhofila > 1)
+        {
+            filaAux->elementos[posicao] = filaAux->elementos[filaAux->primeiro] ;
+            filaAux->elementos[filaAux->primeiro] = 0 ;
+            filaAux->primeiro = filaAux->primeiro + 1 ;
+            filaAux->ultimo = posicao ;
+        }
+    }else
+        printf ("vazia\n") ;
+}
+
+void listarFila(tipoFC *filaAux) {
+  int i = filaAux->ultimo ;
+  printf("Lista Normal:\n") ;
+  for (int j = 0 ; j < filaAux->tamanhofila ; j++)
+  {
+    printf ("%d ", filaAux->elementos[i]) ;
+    i-- ;
+  }
+  printf ("\n\n") ;
+}
+
+void listarFila2(tipoFC *filaAux)
+{
+    int i ;
+    printf("Lista inteira:\n") ;
+    for (i = 0 ; i < 15 ; i++)
+    {
+      printf ("%d ", filaAux->elementos[i]) ;
+    }
+    printf("\n\n") ;
+}
+
+int main ()
+{
+    tipoFC filaEsC ;
+    int i, x, op, valor ;
+    scanf ("%d", &x) ;
+    if (x > 0 && x <= 100)
+    {
+        inicializaFila(&filaEsC) ;
+        for (i = 0 ; i < x ; i++)
+        {
+            scanf ("%d", &op) ;
+            if (op == 1)
+            {
+                scanf ("%d", &valor) ;
+                insereElemento(&filaEsC, valor) ;
+                listarFila2(&filaEsC) ;
+                listarFila(&filaEsC) ;
+            }
+            if (op == 2)
+            {
+                removerElemento(&filaEsC) ;
+                printf ("Removendo Elemento:\n") ;
+                listarFila2(&filaEsC) ;
+                listarFila(&filaEsC) ;
+            }
+            if (op == 3)
+            {
+                finalizarTarefa(&filaEsC) ;
+                printf ("Finalizando Tarefa:\n") ;
+                listarFila2(&filaEsC) ;
+                listarFila(&filaEsC) ;
+            }
+        }
+    }
+    return 0 ;
+}
